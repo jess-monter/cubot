a4aa227b5a512eaa50d476c65ece933676972344
jess-monter <jessmonter.g@gmail.com>
2025-07-16 14:25:57 -0600

    Add deploy files, docker compose, dockerfile, add entrypoints, pull model on ollama serve

--- cubot/urls.py
+++ cubot/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
@@ -23,3 +25,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.chat.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

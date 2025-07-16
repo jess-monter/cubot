cd55a24c03b3a0f972c7e05247de94fdc3136fe1
jess-monter <jessmonter.g@gmail.com>
2025-07-16 15:30:45 -0600

    Get allowed host from env vars

--- cubot/settings.py
+++ cubot/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv("ALLOWED_HOST", "localhost"),
+]
 
 
 # Application definition
